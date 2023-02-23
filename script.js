9785211ad77e2e81c299d68de86420f4f5f08156
sanjay kumar <70475208+snjy-kumar@users.noreply.github.com>
2023-02-23 18:08:45 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+document.getElementById("submitButton").onclick = function(){
+
+  let temp;
+  
+  if(document.getElementById("cButton").checked){
+    temp = document.getElementById("textBox").value;
+    temp = Number(temp);
+    temp = toCelsius(temp);
+    document.getElementById("tempLabel").innerHTML = temp + " °C";
+    
+  }
+  else if(document.getElementById("fButton").checked){
+    temp = document.getElementById("textBox").value;
+    temp = Number(temp);
+    temp = toFarenheit(temp);
+    document.getElementById("tempLabel").innerHTML = temp + " °F";
+
+  }
+  else{
+    document.getElementById("tempLabel").innerHTML = "Select a Unit";
+  }
+  
+}
+
+function toCelsius(temp){
+  return (temp - 32) * (5/9);
+}
+
+function toFarenheit(temp){
+  return temp * 9 / 5 + 32;
+}
